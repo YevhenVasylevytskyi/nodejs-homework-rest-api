7ad18b2e853ade5eb5dcd18d6f89cf97c9deb8ef
Yevhen Vasylevytskyi <yevhenvasylevytskyi@gmail.com>
2022-01-26 07:44:26 +0200

    add IMEIL_FROM ot .env

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,14 +1,14 @@
 const sgMail = require('@sendgrid/mail')
 require('dotenv').config()
 
-const { SENDGRID_API_KEY } = process.env
+const { SENDGRID_API_KEY, IMEIL_FROM } = process.env
 
 sgMail.setApiKey(SENDGRID_API_KEY)
 
 const sendEmail = async(data, next) => {
     // eslint-disable-next-line no-useless-catch
     try {
-        const email = {...data, from: 'yevhenvasylevytskyi@gmail.com'}
+        const email = {...data, from: IMEIL_FROM}
         await sgMail.send(email)
         return true
     } catch (error) {
